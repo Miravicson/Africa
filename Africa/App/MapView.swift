d0d15b21ca45adc2a852e032f8cc19ba322ef872
miravicson <victorughonu95@gmail.com>
2021-11-26 20:55:22 +0100

    Created the following files. * MainView -- contains the Tab that contains other views * VideoListView * MapView * GalleryView
    Replaced the ContentView in the App's WindowGroup with the MainView

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Victor Ughonu on 26/11/2021.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
