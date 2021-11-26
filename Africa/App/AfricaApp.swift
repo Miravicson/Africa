40c01ee482c9c35d45145daad40ae88d4fb91a5e
miravicson <victorughonu95@gmail.com>
2021-11-26 20:39:43 +0100

    Initial Commit

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Victor Ughonu on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
