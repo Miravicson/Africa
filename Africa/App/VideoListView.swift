f205476a84c880ee6060e4f6d1045cb325a19e37
miravicson <victorughonu95@gmail.com>
2021-11-27 17:54:45 +0100

    Started implementing the Map Tab Section

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct VideoListView: View {
     // MARK: - PROPERTIES
     
-    var videos: [Video] = Bundle.main.decode("videos.json")
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
     
     // MARK: - BODY
     
@@ -18,12 +20,25 @@ struct VideoListView: View {
         NavigationView {
             List {
                 ForEach(videos) { item in
-                    VideoListItemView(video: item)
-                        .padding(.vertical, 8)
+                    NavigationLink(destination: VideoPlayerView(videoSelected: item.id, videoTitle: item.name)) {
+                        VideoListItemView(video: item)
+                            .padding(.vertical, 8)
+                    }
                 } //: LOOP
             } //: LIST
             .listStyle(InsetGroupedListStyle())
             .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        // Shuffle videos
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "shuffle")
+                    }
+                }
+            }
         } //: NAVIGATION
     }
 }
