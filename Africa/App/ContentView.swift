b6544b2c84adb32055766d6475d687897c131516
miravicson <victorughonu95@gmail.com>
2021-11-27 06:58:37 +0100

    Created Detail View and Fixed the Launch Screen bug.

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Content")
-            .padding()
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                ForEach(animals) {animal in
+                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                        AnimalListItemView(animal: animal)
+                    } //: LINK
+                }
+            } //: LIST
+            .navigationBarTitle("Africa", displayMode: .large)
+        } //: NAVIGATION
     }
 }
 
